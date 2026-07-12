8618851e0b37adba5e339019da75fa3188a3cd72
Nikolaj Kashirskij <nikolajkashirskij@gmail.com>
2026-07-12 22:18:27 +0300

    update intel graphics

--- modules/hardware/intel.nix
+++ modules/hardware/intel.nix
@@ -6,5 +6,18 @@
   hardware = {
     enableAllFirmware = true;
     cpu.intel.updateMicrocode = true;
+
+    graphics = {
+      enable = true;
+
+      extraPackages = with pkgs; [
+        intel-media-driver
+        vpl-gpu-rt
+      ];
+    };
+  };
+
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD";
   };
 }
